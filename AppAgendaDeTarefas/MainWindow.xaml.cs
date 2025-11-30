a84b220bd081451ef44ac1e78b0b7d0eeb4d632b
Frederico Martins <fred.robotista@gmail.com>
2025-11-30 12:40:44 -0300

    repositorio com estrutura inicial do projeto

---
+++ AppAgendaDeTarefas/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppAgendaDeTarefas;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+    /// <summary>
+    /// funcionalidade inicial 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Adicionar_Click(object sender, RoutedEventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(TxtTarefa.Text))
+        {
+            ListaTarefas.Items.Add(TxtTarefa.Text);
+            TxtTarefa.Clear();
+        }
+        else
+        {
+            MessageBox.Show("Digite uma tarefa válida!");
+        }
+    }
+}
